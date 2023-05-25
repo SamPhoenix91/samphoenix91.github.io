83af19c6eb729c96b08cab6ef1336229d99efcc7
Sam <sdavis8991@gmail.com>
2023-05-25 18:41:23 +0100

    Actually implimented alt text lol

--- js/footer.js
+++ js/footer.js
@@ -1,14 +1,14 @@
 var html = `
 
 
-<link rel="stylesheet" href="/css/footer.css">
-<input id="menu-toggle" type="checkbox" />
+<link rel="stylesheet" href="./css/footer.css">
 
-<a id="copyright" href="/Terms.html">© Sam Davis 2023</a>
-    <span id="wip">Work in Progress</span>
-    <a id="issuereport" href="https://github.com/SamPhoenix91/samphoenix91.github.io/issues" target="_blank"> If you see
-        any
-        issues, please make a issue report on github</p>
+<a id="copyright" href="Terms.html">© Sam Davis 2023</a>
+<span id="wip">Work in Progress</span>
+<a id="issuereport" href="https://github.com/SamPhoenix91/samphoenix91.github.io/issues" target="_blank"> If you see
+    any
+    issues, please make a issue report on github</p>
+    
 
 `;
 
